5495d6ae858e68f986755db407371b817f6741f4
anton rångeby <antonrangeby@MacBook-Air-som-tillhor-anton-2.local>
2023-01-16 11:45:07 +0100

    steg 2

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var arr = ['apa', 'orangutang', 'gorilla', 'get', 'giraff',];
 console.log(arr);
+var person = {
+    name: "anton",
+    age: 3,
+    canDrink: false
+};
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
 const arr = ['apa', 'orangutang', 'gorilla', 'get', 'giraff',];
 
 console.log(arr)
+
+interface objInterface {
+    name: string;
+    age: number;
+    canDrink: boolean;
+}
+
+const person: objInterface = {
+    name: "anton",
+    age: 3,
+    canDrink: false,
+}
+
+console.log(person)
